a30e141ce6c6feacd0df12f3a48e95d79f4dfd17
Danny Nahurnyi <dnahurnyi@gmail.com>
2018-03-03 16:12:58 +0200

    fixing heroku

--- api/currencies/currencies.controller.js
+++ api/currencies/currencies.controller.js
@@ -6,11 +6,16 @@ const currencyDao = require(`./currencies.dao`)
 
 module.exports.getCurrenciesRoute = async (req, res, next) => {
 
+	console.log('1')
 	const currencies = await currencyUtils.getCurrencyDescriptionsOuterService()
+	console.log('2')
 	const currenciesArray = currencyUtils.prepareToSaveCurrencyDescriptions(currencies)
+	console.log('3')
 	await currencyDao.createCurrencyDescription(currenciesArray)
+	console.log('4')
 
 	const currencValues = await currencyUtils.getCurrencyValuesOuterService()
+	console.log('5')
 	
 
 	res.json(currencies)
