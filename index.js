54ee1e1bf07f76cbdc33ded036d003f27814e87a
Danny Nahurnyi <dnahurnyi@gmail.com>
2018-03-03 11:39:32 +0200

    Setup environment

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express')
+const http = require('http')
+const app = express()
+
+require('./config/express')(app)
+
+// Routing
+app.use('/', require('./routes/api'))
+
+// db connection and settings
+const connection = require('./config/connection')
+connection.getMongoose()
+
+// error-handler settings
+require('./config/errorHandler')(app)
+
+// create server
+app.set('port', process.env.PORT || 3000)
+const server = http.createServer(app)
+server.listen(app.get('port'), () => {
+	console.log(app.get('env'))
+	console.log('listening at:', app.get('port'))
+})
+
+module.exports = app
