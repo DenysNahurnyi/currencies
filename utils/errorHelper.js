121474c85de42773017e36fe85b0cce8dc699b03
Danny Nahurnyi <dnahurnyi@gmail.com>
2018-03-03 11:42:25 +0200

    add util for error handling

---
+++ utils/errorHelper.js
@@ -0,0 +1,34 @@
+'use strict'
+
+class AppError extends Error {
+	constructor (type, message) {
+		if (message instanceof AppError) return message
+		super(message)
+		this.error = type
+		delete this.stack
+	}
+}
+
+module.exports.serverError = (err = '') => {
+	const error = new AppError('SERVER_ERROR', err)
+	error.code = 500
+	return error
+}
+
+module.exports.notFound = (err = '') => {
+	const error = new AppError('NOT_FOUND', err)
+	error.code = 404
+	return error
+}
+
+module.exports.badRequest = (err = '') => {
+	const error = new AppError('BAD_REQUEST', err)
+	error.code = 400
+	return error
+}
+
+module.exports.forbidden = (err = '') => {
+	const error = new AppError('FORBIDDEN', err)
+	error.code = 403
+	return error
+}
